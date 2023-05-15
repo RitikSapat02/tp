33b60961ea10e5442c4a273d3fe226e3668e4b9c
Ritik Sapat <118529770+RitikSapat02@users.noreply.github.com>
2023-05-15 13:17:55 +0530

    Update settings.py

--- hello/settings.py
+++ hello/settings.py
@@ -25,7 +25,8 @@ SECRET_KEY = 'django-insecure-1xs!kkoi1k#8@r#v$xra_v8dq@@zvegs#m*z9rwpmd7h((#_oq
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+
+ALLOWED_HOSTS = ['.vercel.app','.now.sh','127.0.0.1','localhost']
 
 
 # Application definition
@@ -73,12 +74,12 @@ WSGI_APPLICATION = 'hello.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
 
 
 # Password validation
